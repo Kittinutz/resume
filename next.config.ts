44eb51a147c4e118d06f4c7072d19ea0952dfe5f
Kittinut Pramhan <kittinut@Kittinuts-MacBook-Air.local>
2025-08-08 16:38:06 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import { withContentlayer } from 'next-contentlayer2';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: 'export',
 };
 
 export default withContentlayer(nextConfig);
